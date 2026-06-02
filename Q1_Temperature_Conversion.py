d7fc7eea3f692c2d58165961cbdd074cb203879c
sanjana <125641551+Sanjana1812@users.noreply.github.com>
2026-06-02 09:26:45 -0400

    Create Q1_Temperature_Conversion.py

---
+++ Q1_Temperature_Conversion.py
@@ -0,0 +1,11 @@
+def celsius_to_fahrenheit(celsius):        # function to convert celsius into fahrenheit
+
+fahrenheit = (celsius * 9/5) + 32        # applying formula
+
+return fahrenheit                      # returning converted value
+
+print(celsius_to_fahrenheit(0))        # checking output with given values
+
+print(celsius_to_fahrenheit(25))
+
+print(celsius_to_fahrenheit(100))
