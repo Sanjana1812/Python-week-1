847a04539e2387aa2b1333042ddfb990f0555a7d
sanjana <125641551+Sanjana1812@users.noreply.github.com>
2026-06-02 09:28:50 -0400

    Create Q2_Grade_Assignment.py

---
+++ Q2_Grade_Assignment.py
@@ -0,0 +1,31 @@
+def assign_grade(score):          # function to assign grade based on marks
+
+if score < 0 or score > 100:        # check invalid input
+    return "Invalid Score"
+
+elif score >= 90:
+    return "A"
+
+elif score >= 80:
+    return "B"
+
+elif score >= 70:
+    return "C"
+
+elif score >= 60:
+    return "D"
+
+else:
+    return "F"
+
+print(assign_grade(95))     # testing given values
+
+print(assign_grade(85))
+
+print(assign_grade(75))
+
+print(assign_grade(65))
+
+print(assign_grade(55))
+
+print(assign_grade(105))
